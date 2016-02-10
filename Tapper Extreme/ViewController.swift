8e87c6bb16e7dda0478c6e19ac449f46e22e41db
Christopher Reynolds <reynolds87chris@fullsail.edu>
2016-02-10 12:27:23 -0700

    Initial Commit

---
+++ Tapper Extreme/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Tapper Extreme
+//
+//  Created by Christopher Reynolds on 2/10/16.
+//  Copyright © 2016 Smooth Black 6. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
